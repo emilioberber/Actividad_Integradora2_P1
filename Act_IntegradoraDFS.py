95abf631698bf281650d396682164cd23c2d68ff
Moisés Hiram Pineda Campos <moypineda10@gmail.com>
2023-11-15 10:00:22 -0600

    DFS added

--- Act_IntegradoraDFS.py
+++ Act_IntegradoraDFS.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,37 +24,65 @@ x = t * points3D[:, 0]
 y = t * points3D[:, 1]
 points2D = np.column_stack((x, y))
 
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v, cost):
+        self.graph[u].append((v, cost))
+        self.graph[v].append((u, cost))  # Adding this line to make the graph undirected
+
+    def DFSUtil(self, v, visited, destination):
+        visited.add(v)
+        print(v, end=' ')
+
+        if v == destination:
+            print("\nDestination reached!")
+            return True
+
+        for neighbour, cost in self.graph[v]:
+            if neighbour not in visited:
+                if self.DFSUtil(neighbour, visited, destination):
+                    return True
+        return False
+
+    def DFS(self, start, destination=None):
+        visited = set()
+        if not self.DFSUtil(start, visited, destination):
+            print("\nDestination not reached!")
+
+# Function to load a matrix from a file
 def cargar_matriz(nombre_archivo):
-    # Carga del archivo usando numpy.loadtxt
     matriz = np.loadtxt(nombre_archivo, skiprows=1, dtype=int)
     return matriz
 
+# Function to calculate the distance between two points in 3D space
 def calcular_distancia(punto1, punto2):
-    # Fórmula de distancia entre dos puntos en 3D
     return np.sqrt((punto1[0] - punto2[0])**2 + (punto1[1] - punto2[1])**2 + (punto1[2] - punto2[2])**2)
 
+# Function to plot connectivity on a 2D plane
 def graficar_conectividad(ax, matriz, canales, puntos_2d, puntos_3d):
-    # Obtener índices de canales conectados
     conexiones = np.argwhere(matriz == 1)
 
-    # Graficar puntos
     ax.scatter(puntos_2d[:, 0], puntos_2d[:, 1])
 
-    # Dibujar líneas de conexión con pesos (distancias físicas)
     for conexion in conexiones:
-        canal_origen = conexion[0]
-        canal_destino = conexion[1]
-        punto_origen = puntos_2d[canal_origen]
-        punto_destino = puntos_2d[canal_destino]
+        canal_origen = canales[conexion[0]]
+        canal_destino = canales[conexion[1]]
+        punto_origen = puntos_2d[conexion[0]]
+        punto_destino = puntos_2d[conexion[1]]
 
-        # Calcular distancia física en 3D
-        distancia = calcular_distancia(puntos_3d[canal_origen], puntos_3d[canal_destino])
+        distancia = calcular_distancia(puntos_3d[conexion[0]], puntos_3d[conexion[1]])
 
-        # Dibujar línea con etiqueta del peso
         ax.plot([punto_origen[0], punto_destino[0]], [punto_origen[1], punto_destino[1]], 'k-', alpha=0.5)
         ax.text((punto_origen[0] + punto_destino[0]) / 2, (punto_origen[1] + punto_destino[1]) / 2, f'{distancia:.2f}', color='blue')
 
-    # Etiquetar los puntos
+        # Calculate the cost as the average distance between origin and destination points
+        costo = calcular_distancia(puntos_3d[conexion[0]], puntos_3d[conexion[1]])
+
+        # Add the edge to the graph
+        graph.addEdge(canal_origen, canal_destino, costo)
+
     for i in range(len(puntos_2d)):
         ax.text(puntos_2d[i, 0] - 0.02, puntos_2d[i, 1] + 0.025, canales[i])
 
@@ -67,12 +96,18 @@ archivos = ["Lectura_s09.txt", "Memoria_s09.txt", "Operaciones_s09.txt"]
 # S0A = Matriz con 32 electrodos 
 # archivos = ["Lectura_s0a.txt", "Memoria_s0a.txt", "Operaciones_s0a.txt"]
 
-# Crear la figura
+# Create the figure
 fig, axs = plt.subplots(1, len(archivos), figsize=(15, 5))
 
-# Mostrar grafos para cada archivo .txt
 for ax, nombre_archivo in zip(axs, archivos):
     matriz = cargar_matriz(nombre_archivo)
+    
+    # Create a new graph for each file
+    graph = Graph()
+    
+    # Added the missing parts related to points2D and conexiones
+    conexiones = np.argwhere(matriz == 1)
+    
     graficar_conectividad(ax, matriz, channels, points2D, points3D)
     circle = plt.Circle((0, 0), 1.04, color='r', alpha=0.25, fill=False)
     ax.add_patch(circle)
@@ -80,5 +115,27 @@ for ax, nombre_archivo in zip(axs, archivos):
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.axis('equal')
+
+    # Add edges to the graph using channel names
+    for conexion in conexiones:
+        canal_origen = channels[conexion[0]]
+        canal_destino = channels[conexion[1]]
+        punto_origen = points2D[conexion[0]]
+        punto_destino = points2D[conexion[1]]
+
+        distancia = calcular_distancia(points3D[conexion[0]], points3D[conexion[1]])
+
+        ax.plot([punto_origen[0], punto_destino[0]], [punto_origen[1], punto_destino[1]], 'k-', alpha=0.5)
+        ax.text((punto_origen[0] + punto_destino[0]) / 2, (punto_origen[1] + punto_destino[1]) / 2, f'{distancia:.2f}', color='blue')
+
+        # Calculate the cost as the average distance between origin and destination points
+        costo = calcular_distancia(points3D[conexion[0]], points3D[conexion[1]])
+
+        # Add the edge to the graph
+        graph.addEdge(canal_origen, canal_destino, costo)
+
+    # Perform DFS traversal from 'Fz' to 'PO8' on the current graph
+    print(f"\nDFS Traversal for {nombre_archivo}:")
+    graph.DFS('Fz', destination='PO8')
 
 plt.show()
