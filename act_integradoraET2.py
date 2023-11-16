6b329a9c4d5ad2d7a27c9bc0866f0e36a95ba445
Moisés Hiram Pineda Campos <moypineda10@gmail.com>
2023-11-16 07:31:30 -0600

    Print DFS Updated

--- act_integradoraET2.py
+++ act_integradoraET2.py
@@ -64,7 +64,7 @@ class Graph:
         current_path.append(v)
 
         if v == destination:
-            print("", ' '.join(current_path))
+            print(' '.join(current_path), " ")
             print("Destination reached!")
             return True
 
