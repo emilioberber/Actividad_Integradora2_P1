b01051d7fe42f51c56c47e84f3fe06e22fba1d8d
Emilio Berber <emilioberber@hotmail.com>
2023-11-21 08:18:24 -0600

    Adding file for ET5

---
+++ act_integradoraET5.py
@@ -0,0 +1,11 @@
+## SAM AQUÍ AGREGA TU CÓDIGO:
+"""
+Un análisis típico que se hace en conectividad funcional es contar cuantas aristas están conectados a cada vértice. En esta etapa utilizarás diagramas de Voronoi para representar dichos valores Para ello:
+
+1. Calcula el grado de cada vértice de tus grafos no conectados (número de aristas de cada vértice).
+2. Para cada grafo, construye un diagrama de Voronoi en el que los puntos los determinan las posiciones de los electrodos en 2D.
+3. Pinta cada región de los diagramas con un color que represente el número de aristas conectadas a cada vértice. Para asignar colores, utiliza una escada de colores adecuada.
+4. Si puedes, dibuja un círculo de radio 1 centrado en el origen, el cual representa la cabeza.
+"""
+
+## AGARRA los grafos de la ET1 puedes quitarle la ponderación o no
